b44ca2c5482a7eb24547db7d93f102b58b66dd0d
Ben Davis <benjamin.davis0705@gmail.com>
2024-04-21 00:52:20 +0200

    Added comments to AlertGenerator.java

--- src/main/java/com/cardiogenerator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardiogenerator/outputs/FileOutputStrategy.java
@@ -1,4 +1,5 @@
-package com.cardio_generator.outputs;
+// Removed underscore and changed file name
+package com.cardiogenerator.outputs;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,32 +8,32 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class fileOutputStrategy implements OutputStrategy {
+public class FileOutputStrategy implements OutputStrategy {
+    //Changed variable name to camelCase
+    private String baseDirectory;
+    //Changed variable name to camelCase
+    public final ConcurrentHashMap<String, String> fileMap = new ConcurrentHashMap<>();
 
-    private String BaseDirectory;
+    public FileOutputStrategy(String baseDirectory) {
 
-    public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
-
-    public fileOutputStrategy(String baseDirectory) {
-
-        this.BaseDirectory = baseDirectory;
+        this.baseDirectory = baseDirectory;
     }
 
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         try {
             // Create the directory
-            Files.createDirectories(Paths.get(BaseDirectory));
+            Files.createDirectories(Paths.get(baseDirectory));
         } catch (IOException e) {
             System.err.println("Error creating base directory: " + e.getMessage());
             return;
         }
         // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
+        String FilePath = fileMap.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString());
 
         // Write the data to the file
         try (PrintWriter out = new PrintWriter(
-                Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+                Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))){
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
         } catch (Exception e) {
             System.err.println("Error writing to file " + FilePath + ": " + e.getMessage());
